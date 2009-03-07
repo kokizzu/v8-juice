779d1df0710293c62ffc3b54d0d18d7f9d751cd3
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-07 17:52:37 +0000

    Added juice::ThrowException() and the whprintf() code to support it.

--- src/include/v8/juice/juice.h
+++ src/include/v8/juice/juice.h
@@ -1,13 +1,33 @@
 #ifndef CODE_GOOGLE_COM_V8_JUICE_JUICE_H_INCLUDED
-#defile CODE_GOOGLE_COM_V8_JUICE_JUICE_H_INCLUDED
-// This file only here for doxygen's benefit.
+#define CODE_GOOGLE_COM_V8_JUICE_JUICE_H_INCLUDED
+
+#include <v8.h>
 namespace v8 {
 
     /**
        The juice namespace is the top-most namespace used by the
        v8-juice toolkit.
     */
-    namespace juice {
+    namespace juice
+    {
+	using ::v8::Handle;
+	using ::v8::Value;
+
+	/**
+	   A convenience function to throw a v8 exception from a
+	   printf-style formatted string.
+
+	   The return value is the result of calling
+	   v8::ThrowException(String::New(the_formatted_string)).
+
+	   Example:
+
+	   \code
+	   return Toss("Error #%d while performing action %s!", errCode, actionType);
+	   \endcode
+	*/
+	Handle<Value> ThrowException(char const * fmt,...);
+
     }
 }
 
