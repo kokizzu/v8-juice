e3c7d348519502c2129534c59620b29c5ef6f0e5
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-07 17:52:37 +0000

    Added juice::ThrowException() and the whprintf() code to support it.

---
+++ trunk/src/lib/juice/juice.cc
@@ -0,0 +1,38 @@
+#include <v8/juice/juice.h>
+
+#include <ostream>
+#include <sstream>
+#include "whprintf.h"
+namespace v8 { namespace juice {
+
+    using namespace v8;
+
+    static long ostream_appender( void * arg,
+				  char const * data,
+				  long n )
+    {
+	std::ostream * os = static_cast<std::ostream *>(arg);
+	if( ! os ) return -1;
+	long i = 0;
+	for( ; i < n; ++i )
+	{
+	    *os << data[i];
+	}
+	return i;
+    }
+
+    Handle<Value> ThrowException( const char *format, ... )
+    {
+	if( ! format || !*format )
+	{
+	    return ThrowException(String::New("Empty exception text passed to v8::juice::Toss()!"));
+	}
+	va_list vargs;
+	va_start( vargs, format );
+	std::ostringstream os;
+	::whprintf( ostream_appender, &os, format, vargs );
+	va_end(vargs);
+	return ThrowException(String::New(os.str().data(),static_cast<int>(os.str().size())));
+    }
+
+}}
