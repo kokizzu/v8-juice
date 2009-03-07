473750f1bc364463fafa58d341154158e1973a0c
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-07 02:11:07 +0000

    plugin-config.h is now auto-generated by configure process.

--- trunk/src/lib/juice/plugin.dl.cpp
+++ trunk/src/lib/juice/plugin.dl.cpp
@@ -1,11 +1,11 @@
 ////////////////////////////////////////////////////////////////////////
 // platform-specific parts of plugin.{c,h}pp: for libltdl and libdl
 ////////////////////////////////////////////////////////////////////////
-#if v8_p3_plugin_CONFIG_HAVE_LIBLTDL // prefer libltdl, but if it's not available...
+#if v8_juice_plugin_CONFIG_HAVE_LIBLTDL // prefer libltdl, but if it's not available...
 #  include <ltdl.h>
 #else           // assume libdl is.
 #  include <dlfcn.h> // this actually has a different name on some platforms!
-#endif // v8_p3_plugin_CONFIG_HAVE_LIBLTDL
+#endif // v8_juice_plugin_CONFIG_HAVE_LIBLTDL
 
 namespace v8 { namespace juice { namespace plugin {
 
@@ -28,7 +28,7 @@ namespace v8 { namespace juice { namespace plugin {
 	    // They open the main() app and are
 	    // required. If they are not called at some
 	    // point then loading DLLs will not work.
-#if v8_p3_plugin_CONFIG_HAVE_LIBLTDL
+#if v8_juice_plugin_CONFIG_HAVE_LIBLTDL
 	    lt_dlinit();
 	    lt_dlopen( 0 );
 #else
@@ -40,7 +40,7 @@ namespace v8 { namespace juice { namespace plugin {
 
 	//CERR << "s11n::plugin::open("<<basename<<"): trying to open: " << where << "\n";
 
-#if v8_p3_plugin_CONFIG_HAVE_LIBLTDL // libltdl:
+#if v8_juice_plugin_CONFIG_HAVE_LIBLTDL // libltdl:
 	soh = lt_dlopen( where.c_str() );
 #else // libdl:
 	soh = dlopen( where.c_str(), RTLD_NOW | RTLD_GLOBAL );
@@ -48,7 +48,7 @@ namespace v8 { namespace juice { namespace plugin {
 	if( 0 == soh )
 	{
 	    const char * err = 0;
-#if v8_p3_plugin_CONFIG_HAVE_LIBLTDL // libltdl:
+#if v8_juice_plugin_CONFIG_HAVE_LIBLTDL // libltdl:
 	    err = lt_dlerror();
 #else // libdl:
 	    err = dlerror();
