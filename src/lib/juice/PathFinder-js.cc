997f5dd5af01cce25382a40060af41f6c5609220
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-11 00:32:27 +0000

    PathFinder bindings now use WeakJSClassCreator for class generation.

--- src/lib/juice/PathFinder-js.cc
+++ src/lib/juice/PathFinder-js.cc
@@ -2,12 +2,15 @@
 //    License: Public Domain
 
 #include <iostream>
+#include <vector>
 
 #include <v8/juice/PathFinder.h>
 #include <v8.h>
 #include <v8/juice/convert.h>
 #include <v8/juice/plugin.h>
 #include <v8/juice/cleanup.h>
+#include <v8/juice/JSClassCreator.h>
+#include <v8/juice/WeakJSClassCreator.h>
 
 #include <iostream> /* only for debuggering */
 #ifndef CERR
@@ -19,88 +22,67 @@ namespace v8 { namespace juice {
     using namespace ::v8::juice::convert;
 #define JSTR(X) String::New(X)
 #define TOSS(X) ThrowException(JSTR(X))
-    static const void * pf_bind_cx() { static const int x=7;return &x;}
 
-    //! Internal util to reduce code duplication elsewhere: unbinds and deletes pf.
-    static void pf_cleanup( void * obj, PathFinder * pf )
+    template <>
+    struct WeakJSClassCreatorOps<PathFinder>
     {
-	//CERR << "pf_cleanup( void@"<<obj<<",  PathFinder@"<<pf<<")\n";
-	if( pf )
+	enum { ExtraInternalFieldCount = 0 };
+	typedef PathFinder WrappedType;
+	static WrappedType * Ctor( Arguments const & argv,
+				   std::string & exceptionText)
 	{
-	    UnbindNative( pf_bind_cx(), obj, pf );
-	    if( pf != &::v8::juice::plugin::PluginPath() )
+	    const int argc = argv.Length();
+	    if( (1 == argc) && argv[0]->IsExternal() )
 	    {
-		delete pf;
+		External * ex = External::Cast( *argv[0] );
+		if( ex )
+		{
+		    return static_cast<PathFinder*>(ex->Value());
+		}
+		else
+		{
+		    exceptionText = "First argument to ctor failed External::Cast()";
+		    return 0;
+		}
+	    }
+	    std::string a0 = (argc>0) ? JSToStdString(argv[0]) : "";
+	    std::string a1 = (argc>1) ? JSToStdString(argv[1]) : "";
+	    std::string a2 = (argc>2) ? JSToStdString(argv[2]) : ":";
+	    //CERR << "PathFinder(["<<a0<<"], ["<<a1<<"], ["<<a2<<"])\n";
+	    PathFinder * pf = new PathFinder( a0, a1, a2 );
+	    if( pf )
+	    {
+		if( pf != &plugin::PluginPath())
+		{
+		    cleanup::AddToCleanup(pf, cleanup_callback );
+		}
+		bind::BindNative( 0, pf, pf );
+	    }
+	    return pf;
+	}
+
+	static void Dtor( WrappedType * obj )
+	{
+	    //CERR << "Dtor() passing on @"<<obj<<'\n';
+	    if( obj && obj != &plugin::PluginPath())
+	    {
+		bind::UnbindNative( 0, obj, obj );
+		cleanup::RemoveFromCleanup(obj);
+		delete obj;
 	    }
 	}
-    }
-
-    /**
-       Destructor for use with v8::juice::cleanup::AddToCleanup().
-       obj MUST be-a (PathFinder*).
-    */
-    static void pf_cleanup( void * obj )
-    {
-	return pf_cleanup( obj, static_cast<PathFinder*>( obj ) );
-    }
-
-    static void pf_dtor(Persistent< Value > object, void *parameter)
-    {
-	::v8::juice::cleanup::RemoveFromCleanup(parameter);
-	pf_cleanup(parameter);
-	object.Dispose();
-	object.Clear();
-    }
-
-    static Persistent<Object> pf_wrap( Handle<Object> _self, PathFinder * value )
-    {
-	if( value != &::v8::juice::plugin::PluginPath() )
+    private:
+	/** Callback for use with juice::cleanup::AddToCleanup(). */
+	static void cleanup_callback( void * obj )
 	{
-	    ::v8::juice::cleanup::AddToCleanup(value, pf_cleanup);
+	    Dtor( static_cast<WrappedType*>(obj) );
 	}
-	BindNative( pf_bind_cx(), value, value );
-	Persistent<Object> self( Persistent<Object>::New(_self) );
-	self.MakeWeak( value, pf_dtor );
-	self->SetInternalField(0,External::New(value));
-	//CERR << "Wrapped PathFinder @"<<value<<" in a Persistent<Object>.\n";
-	return self;
-    }
 
-    static Handle<Value> pf_ctor(const Arguments& argv)
-    {
-	if (!argv.IsConstructCall()) 
-	    return ThrowException(String::New("Cannot call PathFinder constructor as function"));
-
-	const int argc = argv.Length();
-	if( (1 == argc) && argv[0]->IsExternal() )
-	{
-	    External * ex = External::Cast( *argv[0] );
-	    if( ex )
-	    {
-		return pf_wrap( argv.This(), static_cast<PathFinder*>(ex->Value()) );
-	    }
-	    else
-	    {
-		return ThrowException(String::New("First argument to ctor failed External::Cast()"));
-	    }
-	}
-	std::string a0 = (argc>0) ? JSToStdString(argv[0]) : "";
-	std::string a1 = (argc>1) ? JSToStdString(argv[1]) : "";
-	std::string a2 = (argc>2) ? JSToStdString(argv[2]) : ":";
-	//CERR << "PathFinder(["<<a0<<"], ["<<a1<<"], ["<<a2<<"])\n";
-	PathFinder * pf = new PathFinder( a0, a1, a2 );
-	//CERR << "PathFinder(["<<a0<<"], ["<<a1<<"], ["<<a2<<"])\n";
-	return pf_wrap( argv.This(), pf );
-    }
-    static PathFinder * pf_cast( Local< Value > v )
-    {
-	if( v.IsEmpty() || ! v->IsObject() ) return 0;
-	Local<Object> o( Object::Cast( *v ) );
-	return GetBoundNative<PathFinder>( pf_bind_cx(), o->GetInternalField(0) );
-    }
+    };
 
 #define ARGS(FUNC,COND) const int argc = argv.Length(); if( !(COND) ) TOSS(FUNC "(): argument assertion failed: " # COND)
-#define PH(H) PathFinder * p = pf_cast( H )
+    //#define PH(H) PathFinder * p = pf_cast( H )
+#define PH(H) PathFinder * p = WeakJSClassCreator<PathFinder>::GetSelf( H )
 #define PHT(H) PH(H); if( ! p ) TOSS("Native PathFinder pointer is null!")
 #define PHV(H) PH(H); if( ! p ) return
 
@@ -165,50 +147,46 @@ namespace v8 { namespace juice {
 #undef PHT
 #undef PHV
 #undef PH
-    Handle<Value> SetupPathFinderClass(const Handle<Object> target )
+    Handle<Value> SetupPathFinderClass(Handle<Object> target )
     {
-	// Set up prototype:
-	Handle<FunctionTemplate> ctor = FunctionTemplate::New(pf_ctor);
-	Local<ObjectTemplate> objt = ctor->InstanceTemplate();
-	objt->SetInternalFieldCount(1);
-	objt->SetAccessor(JSTR("pathString"),
-			  pf_get_path_string,
-			  pf_set_path_string
-			  );
-	objt->SetAccessor(JSTR("pathArray"),
-			  pf_get_path_array,
-			  pf_set_path_array
-			  );
-	objt->SetAccessor(JSTR("pathSeparator"),
-			  pf_get_path_sep,
-			  pf_set_path_sep
-			  );
+	HandleScope scope;
+	typedef WeakJSClassCreator<PathFinder> PW;
+	PW pw( "PathFinder", target );
+	pw.Set("shared", Handle<Object>(Object::New())).
+	    Set("pathString",
+		pf_get_path_string,
+		pf_set_path_string
+		).
+	    Set("pathArray",
+		pf_get_path_array,
+		pf_set_path_array
+		).
+	    Set("pathSeparator",
+		pf_get_path_sep,
+		pf_set_path_sep
+		).
+	    Set("find", pf_find );
 
-	objt->Set(JSTR("find"), FunctionTemplate::New( pf_find ) );
-	// Install ctor:
-	Handle<Function> pffunc( ctor->GetFunction() );
-	target->Set(String::New("PathFinder"), pffunc );
-
-	// Install an instance wrapping the v8::juice::plugin::PluginPath() shared object:
-	Handle<Value> pfex( External::New( &::v8::juice::plugin::PluginPath() ) );
-
+	Handle<Function> ctorFunc = pw.Seal();
 	Handle<Object> shared = Object::New();
-	pffunc->Set(String::New("shared"), shared );
-
+	ctorFunc->Set(JSTR("shared"),shared);
 	{
-	    // Plugins path:
-	    Handle<Object> pfobj = pffunc->NewInstance( 1, &pfex );
+	    // Install an instance wrapping the v8::juice::plugin::PluginPath() shared object:
+	    Handle<Value> pfex( External::New( &::v8::juice::plugin::PluginPath() ) );
+	    Handle<Object> pfobj = ctorFunc->NewInstance( 1, &pfex );
 	    shared->Set(String::New("plugins"), pfobj );
 	}
 
 	{
-	    // Includes path:
-	    Handle<Value> tmp = JSTR(".:/usr/lib/v8/plugins");
-	    Handle<Object> pfInc = pffunc->NewInstance( 1, &tmp );
+	    // Includes includes path:
+	    std::vector<Handle<Value> > vec(3,Null());
+	    vec[0] = JSTR(".");
+	    vec[1] = JSTR(".js:.v8:.juice:.v8-juice");
+	    vec[2] = JSTR(":");
+	    Handle<Object> pfInc = ctorFunc->NewInstance( 3, &vec[0] );
 	    shared->Set(String::New("include"), pfInc );
 	}
-
-	return pffunc;
+	return target;
     }
 #undef JSTR
 #undef TOSS
