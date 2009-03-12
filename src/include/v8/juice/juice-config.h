315b7212cb766cc6f87842d2b56db14677a584fe
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-12 23:00:06 +0000

    i hate to do this, but: including generated headers into svn so that the scons build will work out of the box (the gen files come from the Make world).

---
+++ src/include/v8/juice/juice-config.h
@@ -0,0 +1,11 @@
+#ifndef code_google_com_p_v8_juice_CONFIG_HPP_INCLUDED
+#define code_google_com_p_v8_juice_CONFIG_HPP_INCLUDED 1
+
+// Compile-time configuration options for the v8::juice toolkit.
+
+#define v8_juice_LIBRARY_VERSION_STRING "20090312"
+#define v8_juice_LIBRARY_VERSION 20090312
+
+#define v8_juice_HOME_PAGE "http://code.google.com/p/v8-juice"
+
+#endif // code_google_com_p_v8_juice_CONFIG_HPP_INCLUDED
