aa7653c82eaa01773c2b15bbae042f1a04a68e80
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-12 20:27:15 +0000

    Corrected subdirs builds. Added src/include as prereq for src/lib so that the new generated header(s) get(s) created before compilation starts.

--- src/client/shell/js/fwd.js
+++ src/client/shell/js/fwd.js
@@ -1,7 +1,9 @@
 var m = new MyNative();
 print('void returns:',m.avoid(),m.avoid1(32),m.avoid2(17,11));
 print(m.hi());
-//print(m.func3(m.func1(),m.func2(m.func1())))
+print(m.func3(m.func1(),m.func2(m.func1())))
 print(m.him(m));
 print(m.me());
+print(m.takes3(2,2,3));
+
 
