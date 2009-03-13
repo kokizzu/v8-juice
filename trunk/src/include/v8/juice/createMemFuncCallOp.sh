03a557df54e2f1037727fd78b07759fc6f7fb234
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-13 20:04:26 +0000

    Reworked the ClassBinder code generation a bit. i think i've got all member func signatures covered (up to 5 args), including const/void.

--- trunk/src/include/v8/juice/createMemFuncCallOp.sh
+++ trunk/src/include/v8/juice/createMemFuncCallOp.sh
@@ -5,9 +5,11 @@
 count=${1-0}
 
 test "$count" -gt 0 || {
-    echo "Usage: $0 NUMBER (>0)"
+    echo "Usage: $0 NUMBER (>0) [COMMAND=BindMemFunc]"
     exit 1
 }
+
+command=${2-BindMemFunc}
 
 tmplsig="typename WrappedType, typename RV, "
 ttlist="WrappedType, RV,"
@@ -135,67 +137,17 @@ echo
 return
 }
 
-function doIt()
+function makeBindFunc()
 {
-    local count=$1
-    if [[ "x$2" = "x" ]]; then
-    ####################
-    # Primary ${callop} template:
-    {
-	echo "/** Member function call forwarder for const member functions taking ${count} arguments. */"
-	echo "template < ${tmplsig} >"
-	echo -e "struct ${callop}\n{"
-	cat <<EOF
-	enum { Arity = $count };
-	typedef WrappedType Type;
-	static Handle<Value> Call( Type CONSTNESS * obj, Arguments const & argv )
-	{
-	    if( ! obj ) return ThrowException(String::New("${callop}::Call(): Native object is null!"));
-	    else if( argv.Length() < Arity ) return ThrowException(String::New("${callop}::Call(): wrong argument count!"));
-		return convert::CastToJS( (RV) (obj->*Func)(
-EOF
-        for (( at = 0; at < count; at = at + 1)); do
-	    local T="A${at}"
-	    echo -en "\t\tconvert::CastFromJS< ${T} >(argv[${at}])"
-	    test $at -ne $((count-1)) && echo "," || echo
-	done
-	echo -e ") );\n}\n};"
-        #echo aTDecl=$aTDecl
-        #echo aTParam=$aTParam
-        ####################
-        # void specialization for ${callop} template:
-        echo "template < ${tmplsigV} >"
-	echo -e "struct ${callop}< WrappedType,void,${aTParam}, Func >\n{"
-	cat <<EOF
-	enum { Arity = $count };
-	typedef WrappedType Type;
-	static Handle<Value> Call( Type CONSTNESS * obj, Arguments const & argv )
-	{
-	    if( ! obj ) return ThrowException(String::New("${callop}::Call(): Native object is null!"));
-	    else if( argv.Length() < Arity ) return ThrowException(String::New("${callop}::Call(): wrong argument count!"));
-            (obj->*Func)(
-EOF
-        for (( at = 0; at < count; at = at + 1)); do
-	    local T="A${at}"
-	    echo -en "\t\tconvert::CastFromJS< ${T} >(argv[${at}])"
-	    test $at -ne $((count-1)) && echo "," || echo
-	done
-	echo -e "\t\t);\n\t\treturn ::v8::Undefined();\n\t}\n};"
-	echo
-}
-    return
-fi # ${callop} gen
-
-    # ClassBinder::BindMemFunc:
-    tmplsig=$(echo $tmplsig | sed -e 's/typename WrappedType,//')
 cat <<EOF
 /**
-   Overload requiring a CONSTNESS WrappedType member function taking ${count} args and returning RV.
+   Overload requiring a WrappedType CONSTNESS member function
+   taking ${count} args and returning RV.
 */
-template < ${tmplsig} >
+template < typename RV, ${aTDecl}, RV ${funcSig} CONSTNESS >
 ClassBinder & BindMemFunc( char const * name )
 {
-    typedef Detail::${callop}< WrappedType, RV, ${aTParam}, Func > Caller;
+    typedef Detail::${callop}< CONSTNESS WrappedType, RV, ${aTParam}, Func > Caller;
     this->Set(name, Detail::MemFuncCallOp< Caller, RV >::Call );
     return *this;
 }
@@ -211,5 +163,19 @@ false && {
     echo tmplsigV=${tmplsigV}
     echo tmplspecV=${tmplspecV}
 }
-makeOps1
-makeOpsConst
+
+
+case $command in
+    BindMemFunc)
+        makeBindFunc | sed -e 's/CONSTNESS //g'
+	makeBindFunc | sed -e 's/CONSTNESS /const /g'
+	;;
+    MemFuncCallOps)
+	    makeOps1
+	    makeOpsConst
+	    ;;
+    *)
+	echo "Unhandled command: ${command}"
+	exit 2
+    ;;
+esac
