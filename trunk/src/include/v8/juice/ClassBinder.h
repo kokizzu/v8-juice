665eac2acf5588c41333725fec71050cba294f0e
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-14 05:45:58 +0000

    Relicenses ClassBinder as public domain.

--- trunk/src/include/v8/juice/ClassBinder.h
+++ trunk/src/include/v8/juice/ClassBinder.h
@@ -1,40 +1,15 @@
 #if !defined(CODE_GOOGLE_COM_P_V8_JUICE_CLASSBINDER_H_INCLUDED)
 #define CODE_GOOGLE_COM_P_V8_JUICE_CLASSBINDER_H_INCLUDED 1
 /*
+   A JS/C++ class binding mechanism based on the juice::convert framework.
+
    Author: Stephan Beal (http://wanderinghorse.net/home/stephan/)
 
-   License: same as v8 (see below)
+   License: Public Domain
 */
-// Copyright 2009 Stephan Beal. All rights reserved.  Redistribution
-// and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
-//     * Neither the name of Google Inc. nor the names of its
-//       contributors may be used to endorse or promote products derived
-//       from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <v8.h>
-
 #include <v8/juice/WeakJSClassCreator.h>
-#include <v8/juice/forwarding.h>
 
 namespace v8 {
 namespace juice {
@@ -49,6 +24,9 @@ namespace juice {
 	   A helper type for forwarding JS arguments to member functions taking 0
 	   arguments. The classes MemFuncCaller(1..N) are generated but follow
 	   this class' API.
+
+	   All variants of this class except the nullary one throw if the argument
+	   list does not have at least the required number of parameters.
 	*/
 	struct MemFuncCaller0
 	{
