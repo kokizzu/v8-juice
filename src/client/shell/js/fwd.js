51acd18df324e53ee0137451410d6f665b8ad3e8
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-16 21:23:34 +0000

    Added demonstration/test of binding member variables.

--- src/client/shell/js/fwd.js
+++ src/client/shell/js/fwd.js
@@ -5,5 +5,14 @@ print(m.func3(m.func1(),m.func2(m.func1())))
 print(m.him(m));
 print(m.me());
 print(m.takes3(2,2,3));
+print('m.str =',(m.str='bye, world!'));
+print('m.other =',m.other);
+m.other = new MyNative();
+// print("Setting other to null.");
+//m.other = null; // .other is marked as R/O: shouldn't do anything
+//print("Set other to null.");
+m.other.str = "i am the other!";
+//print("Re-assigned other.str.");
+print('m.other.str =',m.other.str);
 
 
