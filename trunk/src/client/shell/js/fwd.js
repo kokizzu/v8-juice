e97e9e7d375cc7ba57629afb96833a60a56c3b7d
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-03-29 02:23:31 +0000

    The WeakJSClassCreator cast operators can now cast from JS to (T&) and (T const &), but throw a std::exception if the conversion fails (to avoid dereferencing null).

--- trunk/src/client/shell/js/fwd.js
+++ trunk/src/client/shell/js/fwd.js
@@ -1,6 +1,8 @@
 var m = new MyNative();
 print('void returns:',m.avoid(),m.avoid1(32),m.avoid2(17,11));
 print(m.hi());
+print(m.func1());
+print(m.func2(m.func1()));
 print(m.func3(m.func1(),m.func2(m.func1())))
 print(m.him(m));
 print(m.me());
@@ -15,5 +17,16 @@ m.other.str = "i am the other!";
 //print("Re-assigned other.str.");
 print('m.other.str =',m.other.str,m.other.me());
 print('forwarder() =',m.other.forwarder(12,-13,8));
-
+print('someref() =',m.someref(m.other));
+var ex = null;
+try
+{
+    print('someref(non-MyNative) =',m.someref(m.other.str));
+}catch(e)
+{
+    ex = e;
+    print("Caught expected exception:",e);
+}
+if( null === ex ) throw("The last test should have thrown but didn't!");
+print("Done!");
 
