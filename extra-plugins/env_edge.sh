152e82e97f6457a9dba0f4602891d3e35ca90e42
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-10-16 17:30:20 +0000

    egg

---
+++ extra-plugins/env_edge.sh
@@ -0,0 +1,13 @@
+# to be sourced by shell code. Sets up the PATH and libpath
+# to use the edge-branch v8-juice library and shell.
+
+edge=$(cd ../branches/edge >/dev/null 2>&1 && pwd)
+if [ x = "x$edge" ]; then
+    echo "'edge' branch dir not found in ../branches!"
+else
+    PATH="$edge/src/client/shell:$PATH"
+    LD_LIBRARY_PATH="$edge/src/lib/juice:${LD_LIBRARY_PATH}"
+    export PATH LD_LIBRARY_PATH
+fi
+unset edge
+
