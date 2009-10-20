03acdf4db020c0181d83e1cdb2a729187855e510
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-10-20 19:27:19 +0000

    Added highly experimental/volatile code for new class binder approach.

---
+++ src/client/sample/BoundNative/BoundNative.js
@@ -0,0 +1,40 @@
+function createThem(count)
+{
+    print("Constructing",count,"BoundNative objects.");
+    function interruptionPoint()
+    {
+        mssleep(1);
+    }
+    var b;
+    for( var i = 0; i < count;++i )
+    {
+        b = new BoundNative();
+        //delete b;
+        if( 0 == (i%333) )
+        {
+            print("Interruption point.");
+            //gc();
+            interruptionPoint();
+        }
+        
+    }
+    print('b ==',b,'typeof b ==',typeof b);
+    print("Constructed",count,"BoundNative objects.");
+    return b;
+}
+var ar = [];
+var b = null;
+var total = 0;
+
+for( var i = 0; i < 5; ++i )
+{
+    var c = 2;
+    print("Creating",c,"objects...");
+    b = createThem(c);
+    total += c;
+    //ar.push( b ); // keep a reference to ensure that gc doesn't reap it.
+    print("b ==",b);//"b.foo ==",b.foo);
+    //gc();
+    print("Created",c,"objects...");
+}
+print("Done. Created",total,"objects in total.");
