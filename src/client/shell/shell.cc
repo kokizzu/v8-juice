e236f20003e558d24058ad1a748c2416bb8437f3
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-10-20 17:47:04 +0000

    Re-ordered two arguments to JuiceShell::InputLoop(), for consistency with ExecuteString().

--- src/client/shell/shell.cc
+++ src/client/shell/shell.cc
@@ -341,7 +341,7 @@ void RunShell( v8::juice::JuiceShell & shell, std::ostream * out )
 {
     v8::TryCatch jtry;
     if( out ) (*out) << "V8 version "<< v8::V8::GetVersion() << '\n';
-    shell.InputLoop( v8::juice::JuiceShell::StdinLineFetcher, out, &jtry );
+    shell.InputLoop( v8::juice::JuiceShell::StdinLineFetcher, &jtry, out );
     if( out ) (*out) << std::endl;
 }
 
