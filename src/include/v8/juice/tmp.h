087b71ab2e2d3d8205273b5346dbe4fb029676fc
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2009-10-30 19:42:45 +0000

    Added tmp::ConstVal, IntVal, and BoolVal. Fixed a typo.

--- src/include/v8/juice/tmp.h
+++ src/include/v8/juice/tmp.h
@@ -11,7 +11,7 @@ namespace v8 { namespace juice {
 namespace tmp {
 
     /**
-       An utmost most-based compile-time assertion template.
+       An utmost most-basic compile-time assertion template.
        If Condition is false, an incomplete specialization of
        this type is invoced, causing a compile-time error.
     */
@@ -23,6 +23,20 @@ namespace tmp {
     /** Unimplemented - causes a compile-time error if used. */
     template <>
     struct Assertion<false>;
+
+
+    /** A convenience base type for metafunctions holding a constant value. */
+    template <typename ValType,ValType Val>
+    struct ConstVal
+    {
+        enum { Value = Val };
+    };
+    /** A metafunction holding an integer constant. */
+    template <int Val>
+    struct IntVal : ConstVal<int,Val> {};
+    /** A metafunction holding a bool constant. */
+    template <bool Val>
+    struct BoolVal : ConstVal<bool,Val> {};
 
     /** A metatype whos Value member is true if X and Y are the same type. */
     template <typename X,typename Y>
@@ -36,6 +50,7 @@ namespace tmp {
     {
         enum { Value = 1 };
     };
+
     
 }}} // namespaces
 
