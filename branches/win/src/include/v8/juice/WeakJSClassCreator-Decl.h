4c79650dd96e53173d09cd31bf674bfc8baa69a6
anton.yemelyanov@gmail.com <anton.yemelyanov@gmail.com@4ecabecc-0a76-11de-b55b-d397e7115e68>
2011-04-08 04:12:14 +0000

    Adding WeakJSClassCreator-Decl.h that allows simple instantiation of WeakJSClassCreator targeted toward a specific class type. This is useful when declaring many similar type wrappers.

---
+++ branches/win/src/include/v8/juice/WeakJSClassCreator-Decl.h
@@ -0,0 +1,54 @@
+//
+// Helper file for generic WeakJSClassCreatorOps instantiation
+//
+
+#if !defined(WEAK_CLASS_TYPE) || !defined(WEAK_CLASS_NAME)
+#error "You must define WEAK_CLASS_TYPE and WEAK_CLASS_NAME before including this file!"
+#endif
+
+#ifndef WEAK_CLASS_CLEANUP
+#define WEAK_CLASS_CLEANUP true
+#endif
+
+namespace v8 { namespace juice {
+
+	template <>
+	struct WeakJSClassCreator_Opt_CleanupOwnedObjects<WEAK_CLASS_TYPE> :  WeakJSClassCreator_Opt_Bool<WEAK_CLASS_CLEANUP> {};
+
+	template <>
+	struct WeakJSClassCreatorOps<WEAK_CLASS_TYPE>
+	{
+	public:
+		enum { ExtraInternalFieldCount = 0 };
+		typedef WEAK_CLASS_TYPE WrappedType;
+		static char const * ClassName() { return WEAK_CLASS_NAME; }
+
+#ifdef WEAK_CLASS_PLAIN_CTOR
+		static WrappedType * Ctor( v8::Arguments const & argv, std::string & exceptionText)
+		{
+			return new WrappedType();
+		}
+#else
+		static WrappedType * Ctor( v8::Arguments const & argv, std::string & exceptionText);
+#endif
+
+#ifdef WEAK_CLASS_PLAIN_DTOR
+		static void Dtor(WrappedType *o)
+		{
+			delete o;
+		}
+#else
+		static void Dtor(WrappedType *o);
+#endif
+
+	};
+
+}} // v8::juice
+
+#include <v8/juice/WeakJSClassCreator-CastOps.h>
+
+#undef WEAK_CLASS_TYPE
+#undef WEAK_CLASS_NAME
+#undef WEAK_CLASS_CLEANUP
+#undef WEAK_CLASS_PLAIN_CTOR
+#undef WEAK_CLASS_PLAIN_DTOR
