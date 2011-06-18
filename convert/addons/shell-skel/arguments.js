029420a3ebc60ef7af0c7d54706fe141f5b198ac
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2011-06-18 15:33:09 +0000

    added sample function for generically parsing command-line arguments

--- convert/addons/shell-skel/arguments.js
+++ convert/addons/shell-skel/arguments.js
@@ -1,1 +1,33 @@
+
+arguments.parseArgs = function() {
+    var i, k, v, m, l;
+    var rxNonFlag = /^[^-]+$/; /* -arg */
+
+    var rx0a = /^--?([^=]+)$/; /* -[-]arg */
+    var rx1a = /^--?([^=]+)=(.*)$/; /* -[-]arg=val */
+    var rc = {
+        flags:{},
+        nonFlags:[]
+    };
+    for( i = 0; i < this.length; ++i ) {
+        l = this[i];
+        //print("TRYING ["+i+"]="+l);
+        if( ! l ) continue;
+        else if( rxNonFlag.test(l) ) {
+            rc.nonFlags.push(l);
+        }
+        else if( (m = rx0a.exec(l)) ) {
+            rc.flags[ m[1] ] = true;
+        }
+        else if( (m = rx1a.exec(l)) ) {
+            rc.flags[ m[1] ] = (m[2] || false);
+        }
+        else {
+            // ??? wtf ???
+        }
+    }
+    return rc;
+};
+
 print('arguments ='+JSON.stringify(arguments));
+print('arguments.parseArgs ='+JSON.stringify(arguments.parseArgs()));
