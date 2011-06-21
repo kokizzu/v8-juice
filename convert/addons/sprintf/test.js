91ff67b774438f7f104eb2dbdbbaac6ee4cb5f48
sgbeal <sgbeal@4ecabecc-0a76-11de-b55b-d397e7115e68>
2011-06-21 19:35:51 +0000

    Added another test assert()ion.

--- convert/addons/sprintf/test.js
+++ convert/addons/sprintf/test.js
@@ -4,7 +4,7 @@ function test1() {
     asserteq( 'hi, world', sprintf('%s, %s','hi','world') );
     asserteq( '42.24', sprintf('%2.2f', 42.243) );
     assertThrows( function() {
-       sprintf("%s%d",'hi');
+       sprintf("%s%d",'not enough arguments');
     });
 }
 
